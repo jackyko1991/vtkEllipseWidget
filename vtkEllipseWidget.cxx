c474709869b76422b09753c98d4ecd90a1f81ee0
Jacky <jackkykokoko@gmail.com>
2018-09-13 18:59:24 +0800

    add comment

--- vtkEllipseWidget.cxx
+++ vtkEllipseWidget.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 Program:   Visualization Toolkit
 Module:    vtkEllipseWidget.cxx
-Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen, Ko Ka Long
 All rights reserved.
 See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
 This software is distributed WITHOUT ANY WARRANTY; without even
